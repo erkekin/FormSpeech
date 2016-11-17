f09cfba3d337b660a1a5df3e21dd21e8f32eaa9b
Erk EKİN <eekin@anadolu.edu.tr>
2016-11-17 15:28:00 +0300

    files added

---
+++ FormSpeech/FormSpeech.swift
@@ -0,0 +1,90 @@
+//
+//  FormSpeech.swift
+//  FormSpeech
+//
+//  Created by Erk Ekin on 16/11/2016.
+//  Copyright © 2016 Erk Ekin. All rights reserved.
+//
+
+import Foundation
+
+typealias Pair = [Field:String]
+protocol Iteratable {}
+
+extension RawRepresentable where Self: RawRepresentable {
+    
+    static func iterateEnum<T: Hashable>(_: T.Type) -> AnyIterator<T> {
+        var i = 0
+        return AnyIterator {
+            let next = withUnsafePointer(to: &i) {
+                $0.withMemoryRebound(to: T.self, capacity: 1) { $0.pointee }
+            }
+            if next.hashValue != i { return nil }
+            i += 1
+            return next
+        }
+    }
+}
+
+extension Iteratable where Self: RawRepresentable, Self: Hashable {
+    
+    static func hashValues() -> AnyIterator<Self> {
+        return iterateEnum(self)
+    }
+    
+    static func rawValues() -> [Self.RawValue] {
+        return hashValues().map({$0.rawValue})
+    }
+}
+
+extension String{
+    
+    private func getSubstring(substring1: String, substring2:String?) -> String?{
+        
+        guard let range1 = self.range(of: substring1) else{return nil}
+        
+        let lo = self.index(range1.upperBound, offsetBy: 0)
+        
+        if let sub = substring2 {
+            
+            if let range2 = self.range(of: sub){
+                let hi = self.index(range2.lowerBound, offsetBy: 0)
+                let subRange = lo ..< hi
+                return self[subRange]
+            }else{
+                return nil
+            }
+        }else {
+            
+            let hi = self.endIndex
+            let subRange = lo ..< hi
+            return self[subRange]
+        }
+    }
+    
+    func parse() -> Pair?{
+        
+        let words = Field.rawValues()
+        var output:Pair = [:]
+        
+        for (index, word) in words.enumerated() {
+            
+            let secondIndex = index + 1
+            let secondWord:String? = secondIndex == words.count ? nil: words[secondIndex]
+            
+            if let value = getSubstring(substring1: word, substring2: secondWord),
+                let key = Field(rawValue: word){
+                
+                output[key] = value.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+                
+            }else {
+                return nil
+            }
+        }
+        
+        return output.count == 0 ? nil:output
+        
+    }
+    
+}
+
---
+++ FormSpeech/ViewController.swift
@@ -0,0 +1,199 @@
+//
+//  ViewController.swift
+//  FormSpeech
+//
+//  Created by Erk Ekin on 16/11/2016.
+//  Copyright © 2016 Erk Ekin. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+enum Field: String, Iteratable{
+    
+    case name = "My name is"
+    case surname = "my surname is"
+    case birthPlace = "I was born in"
+    case phoneNumber = "my number is"
+    
+}
+
+class Form{
+    
+    var name:String!
+    var surname:String!
+    var birthPlace:String!
+    var phoneNumber:String!
+    
+    init?(text:String) {
+        
+        print(text)
+        if let output = text.parse(){
+            print(output)
+            name = output[Field.name]
+            surname = output[Field.surname]
+            birthPlace = output[Field.birthPlace]
+            phoneNumber = output[Field.phoneNumber]
+            
+        }else  { return nil}
+        
+    }
+    
+}
+
+class ViewController: UIViewController,SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var surname: UITextField!
+    @IBOutlet weak var birthPlace: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    
+    @IBOutlet private weak var recordBtn : UIButton!
+    
+    private var speechRecognizer: SFSpeechRecognizer!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest!
+    private var recognitionTask: SFSpeechRecognitionTask!
+    private let audioEngine = AVAudioEngine()
+    private let defaultLocale = Locale(identifier: "en-US")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            /*
+             The callback may not be called on the main thread. Add an
+             operation to the main queue to update the record button's state.
+             */
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.recordBtn.isEnabled = true
+                    
+                case .denied:
+                    self.recordBtn.isEnabled = false
+                    self.recordBtn.setTitle("User denied access to speech recognition", for: .disabled)
+                    
+                case .restricted:
+                    self.recordBtn.isEnabled = false
+                    self.recordBtn.setTitle("Speech recognition restricted on this device", for: .disabled)
+                    
+                case .notDetermined:
+                    self.recordBtn.isEnabled = false
+                    self.recordBtn.setTitle("Speech recognition not yet authorized", for: .disabled)
+                }
+            }
+        }
+        
+        speechRecognizer = SFSpeechRecognizer(locale: defaultLocale)!
+        speechRecognizer.delegate = self
+        
+    }
+    private func startRecording() throws {
+        
+        // Cancel the previous task if it's running.
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else { fatalError("Audio engine has no input node") }
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        // Configure request so that results are returned before audio recording is finished
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // A recognition task represents a speech recognition session.
+        // We keep a reference to the task so that it can be cancelled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            print(result)
+            if let result = result {
+                
+                isFinal = result.isFinal
+                if(isFinal == true){
+                    self.parseSpeech(speechText: result.bestTranscription.formattedString)
+                    
+                }
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.recordBtn.isEnabled = true
+                self.recordBtn.setTitle("Start Recording", for: [])
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        try audioEngine.start()
+        
+    }
+    
+    // MARK: - SFSpeechRecognizerDelegate
+    
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            recordBtn.isEnabled = true
+            recordBtn.setTitle("Start Recording", for: [])
+        } else {
+            recordBtn.isEnabled = false
+            recordBtn.setTitle("Recognition not available", for: .disabled)
+        }
+    }
+    
+    // =========================================================================
+    // MARK: - Actions
+    
+    @IBAction func recordBtnTapped() {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordBtn.isEnabled = false
+            recordBtn.setTitle("Stopping", for: .disabled)
+            
+        } else {
+            try! startRecording()
+            recordBtn.setTitle("Stop recording", for: [])
+            
+            name.text = ""
+            surname.text = ""
+            birthPlace.text = ""
+            phoneNumber.text = ""
+        }
+    }
+    
+    func parseSpeech(speechText:String){
+        
+        guard let form = Form(text: speechText) else {return}
+        
+        name.text = form.name
+        surname.text = form.surname
+        birthPlace.text = form.birthPlace
+        phoneNumber.text = form.phoneNumber
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
+
